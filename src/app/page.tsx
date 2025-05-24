3419b0fbc8c245933d45b38a102fc99009282050
PrakashJangid17 <pj177606@gmail.com>
2025-05-24 08:18:49 +0530

    code crection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+
 
 export default function Home() {
   return (
-   <><h1 className="text-bold text-lime-800 flex justify-center text-4xl ">Hello World</h1></>
+   <><h1 className="text-bold text-lime-800 flex justify-center text-4xl  ">Hello World</h1></>
   );
 }
